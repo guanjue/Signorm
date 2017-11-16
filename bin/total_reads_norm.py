1fcd46198447b787977df7733be66b034a4d311e
GUANJUE XIANG <gzx103@E1-052111.science.psu.edu>
2017-11-16 14:29:57 -0500

    total reads norm sig1

--- bin/total_reads_norm.py
+++ bin/total_reads_norm.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def total_reads_norm(sig1, sig2, method, sig2_normed_output):
+def total_reads_norm(sig1, sig2, method, sig1_normed_output):
 	data1 = open(sig1,'r')
 	data2 = open(sig2,'r')
 
@@ -25,22 +25,22 @@ def total_reads_norm(sig1, sig2, method, sig2_normed_output):
 
 	### get total reads scale factor
 	if method == 'mean':
-		scale_factor = np.mean(sig1) / np.mean(sig2)
+		scale_factor = np.mean(sig2) / np.mean(sig1)
 	elif method != 'mean':
-		scale_factor = np.percentile(sig1, float(method)) / np.percentile(sig2, float(method))
+		scale_factor = np.percentile(sig2, float(method)) / np.percentile(sig1, float(method))
 
-	### get normed signal2
-	sig2_normed = np.around(sig2 * scale_factor, decimals = 0)
+	### get normed signal1
+	sig1_normed = np.around(sig1 * scale_factor, decimals = 0)
 
 	### write output
-	result = open(sig2_normed_output,'w')
-	for records in sig2_normed:
+	result = open(sig1_normed_output,'w')
+	for records in sig1_normed:
 		result.write(str(records)+'\n')
 	result.close()
 
 
 ###########################################
-# python total_reads_norm.py -a signal_1_file -b signal_2_file -m mean -o normed_sig2_output_file
+# python total_reads_norm.py -a signal_1_file -b signal_2_file -m mean -o normed_sig1_output_file
 import getopt
 import sys
 
@@ -48,12 +48,12 @@ def main(argv):
 	try:
 		opts, args = getopt.getopt(argv,"ha:b:m:o:")
 	except getopt.GetoptError:
-		print 'python total_reads_norm.py -a signal_1_file -b signal_2_file -m method(mean or percentile #) -o normed_sig2_output_file'
+		print 'python total_reads_norm.py -a signal_1_file -b signal_2_file -m method(mean or percentile #) -o normed_sig1_output_file'
 		sys.exit(2)
 
 	for opt,arg in opts:
 		if opt=="-h":
-			print 'python total_reads_norm.py -a signal_1_file -b signal_2_file -m method(mean or percentile #) -o normed_sig2_output_file'
+			print 'python total_reads_norm.py -a signal_1_file -b signal_2_file -m method(mean or percentile #) -o normed_sig1_output_file'
 			sys.exit()
 		elif opt=="-a":
 			sig1=str(arg.strip())
@@ -62,8 +62,8 @@ def main(argv):
 		elif opt=="-m":
 			method=str(arg.strip())
 		elif opt=="-o":
-			sig2_normed_output=str(arg.strip())
-	total_reads_norm(sig1, sig2, method, sig2_normed_output)
+			sig1_normed_output=str(arg.strip())
+	total_reads_norm(sig1, sig2, method, sig1_normed_output)
 
 if __name__=="__main__":
 	main(sys.argv[1:])
