3a41eca0a4690dc4fbbbe529c333969c46a8e627
Universe <universe@MacBook-Air.local>
2017-11-19 00:31:00 -0500

    add r2 function

--- bin/get_relictes_r2.R
+++ bin/get_relictes_r2.R
@@ -3,7 +3,9 @@ library(LSD)
 args = commandArgs(trailingOnly=TRUE)
 
 input_file_list = args[1]
-output_folder = args[2]
+input_folder = args[2]
+output_folder = args[3]
+
 input_file_names = read.table(input_file_list, header = FALSE, sep='\t')
 
 test_r2 = function(sig1, sig2){
@@ -11,19 +13,23 @@ test_r2 = function(sig1, sig2){
 	return(r2)
 }
 
+r2_vector = c()
+r_vector = c()
 for ( i in seq(dim(input_file_names)) ){
 	sig1_file=toString(input_file_names[i,1])
 	sig2_file=toString(input_file_names[i,2])
 	print(sig1_file)
 	print(sig2_file)
 	### read signal file
-	sig1=read.table(sig1_file, header=FALSE)
-	sig2=read.table(sig2_file, header=FALSE)
+	sig1=read.table(paste(input_folder, sig1_file, sep=''), header=FALSE)
+	sig2=read.table(paste(input_folder, sig2_file, sep=''), header=FALSE)
 	### calculate r2 & r
 	r2 = test_r2(sig1, sig2)
 	print(r2)
+	r2_vector[i] = r2
 	r = cor(sig1, sig2)
 	print(r)
+	r_vector[i] = r
 	### plot scatterplot
 	png(paste(output_folder, sig1_file, sig2_file, '.png', sep=''))
 	heatscatter(sig1[,], sig2[,], log='xy', pch = 20)
@@ -31,6 +37,8 @@ for ( i in seq(dim(input_file_names)) ){
 	dev.off()
 }
 
-
+### write output
+r2r_matrix = cbind(r2_vector, r2_vector)
+write.table(r2r_matrix, paste(input_file_list, '.r2_r.txt', sep=''), quote=FALSE, col.names=FALSE, row.names=FALSE, sep='\t')
 
 
