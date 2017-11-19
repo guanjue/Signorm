fd39c0df6e567abd75bdf83b0f0d7fb8da5cc2f2
Universe <universe@MacBook-Air.local>
2017-11-19 00:22:30 -0500

    add r2 function

--- bin/get_relictes_r2.R
+++ bin/get_relictes_r2.R
@@ -1,8 +1,9 @@
+library(LSD)
 ### get parameters
 args = commandArgs(trailingOnly=TRUE)
 
 input_file_list = args[1]
-
+output_folder = args[2]
 input_file_names = read.table('info_table_compare_r2_signorm.txt', header = FALSE, sep='\t')
 
 test_r2 = function(sig1, sig2){
@@ -10,7 +11,7 @@ test_r2 = function(sig1, sig2){
 	return(r2)
 }
 
-for (i in seq(dim(input_file_names[i,1])[1])){
+for ( i in seq(dim(input_file_names[i,1])) ){
 	sig1_file=toString(input_file_names[i,1])
 	sig2_file=toString(input_file_names[i,2])
 	print(sig1_file)
@@ -23,6 +24,11 @@ for (i in seq(dim(input_file_names[i,1])[1])){
 	print(r2)
 	r = cor(sig1, sig2)
 	print(r)
+	### plot scatterplot
+	png(paste(output_folder, sig1_file, sig2_file, '.png', sep=''))
+	heatscatter(sig1, sig2, log='xy', pch = 20)
+	abline(0, 1, col='red')
+	dev.off()
 }
 
 
