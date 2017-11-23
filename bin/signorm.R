8a7b44e6cbbe524fe9b35f52b26f2b738635fabc
Universe <universe@MacBook-Air.local>
2017-11-23 18:29:02 -0500

    add each each sf method

--- bin/signorm.R
+++ bin/signorm.R
@@ -70,10 +70,10 @@ if (is.element(scale_factor_type, c(1,2,3,4))){
 } else if (scale_factor_type==7) {
 	### only normalize high signal part
 	print('t value norm')
-	### initialize t-r matrix hash	
+	### initialize t-r matrix hash  
 	t_r_hash = hash( t_r_matrix[,1], t_r_matrix[,3] / t_r_matrix[,2] )
 	### give each t a independent sf
-	data_x_sig_norm = as.matrix( apply(cbind(data_x_sig, data_y_sig), 1, function(x) if(x[1]!=0){ x[1] * t_r_hash$toSting(x[1]+x[2]) } else{x[1]} ) )
+	data_x_sig_norm = as.matrix( apply(cbind(data_x_sig, data_y_sig), 1, function(x) if(x[1]!=0){ x[1] * values(t_r_hash[toString(x[1]+x[2])]) } else{x[1]} ) )
 }
 
 
