f2067f2ed7b36fc66c7bac4dd783aae86ee980e1
Universe <universe@MacBook-Air.local>
2017-11-29 01:12:12 -0500

    evaluation

--- bin/evaluation_vector.R
+++ bin/evaluation_vector.R
@@ -25,16 +25,15 @@ test_R2 = function(sig1, sig2){
 d1_od = read.table(s1, header = F)
 d2_od = read.table(s2, header = F)
 
-used_id = as.logical((d1_od!=0)*(d2_od!=0))
-d1_od_no0 = d1_od[used_id,]
-d2_od_no0 = d2_od[used_id,]
-
 ### if sampling_num != 0, sampling calculate scale factor & plotting 
 if (sampling_num != 0){
 	set.seed(seed)
 	used_id = sample(dim(d1_od)[1],sampling_num)
 	d1 = d1_od[used_id,]
 	d2 = d2_od[used_id,]		
+} else{
+	d1 = d1_od
+	d2 = d2_od
 }
 
 ### only keep both nonzero bins
@@ -45,15 +44,17 @@ d2_no0 = d2[used_id]
 ### get sample evaluation
 pearson_cor = cor(d1_no0, d2_no0, method = 'pearson')
 spearman_cor = cor(d1_no0, d2_no0, method = 'spearman')
+pearson_cor_all = cor(d1, d2, method = 'pearson')
+spearman_cor_all = cor(d1, d2, method = 'spearman')
+
 MSE = test_MSE(d1_no0, d2_no0)
 R2 = test_R2(d1_no0, d2_no0)
+
 total_reads_d1 = sum(d1_od)
 total_reads_d2 = sum(d2_od)
-total_reads_d1_no0 = sum(d1_od_no0)
-total_reads_d2_no0 = sum(d2_od_no0)
 
 ### merge all evaluation scores
-result = data.frame(s1, s2, pearson_cor, spearman_cor, MSE, R2, total_reads_d1, total_reads_d2, total_reads_d1_no0, total_reads_d2_no0)
+result = data.frame(s1, s2, pearson_cor, spearman_cor, pearson_cor_all, spearman_cor_all, MSE, R2, total_reads_d1, total_reads_d2)
 
 ### write output
 write.table(result, paste(output_filename, '.txt', sep=''), quote=F, sep='\t', row.names = FALSE, col.names = TRUE)
