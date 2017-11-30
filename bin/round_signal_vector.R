01d4a3ce2508a72aa994d048c40c0f2dab827558
GUANJUE XIANG <gzx103@E1-052111.science.psu.edu>
2017-11-29 20:01:31 -0500

    add poisson

--- bin/round_signal_vector.R
+++ bin/round_signal_vector.R
@@ -8,7 +8,7 @@ round_digit = as.numeric(args[3])
 signal = read.table(input_filename, header = FALSE)
 
 ### round signal
-signal_round = round(a, digit = round_digit)
+signal_round = round(signal, digit = round_digit)
 
 ### write signal vector
 write.table(signal_round, output_filename, quote=FALSE, col.names=FALSE, row.names=FALSE, sep='\t')
