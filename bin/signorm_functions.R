a7a8925e65e20f38d468ebddb1587dc09aa423a9
Universe <universe@MacBook-Air.local>
2017-12-03 16:49:36 -0500

    add round t for robustness

--- bin/signorm_functions.R
+++ bin/signorm_functions.R
@@ -9,10 +9,15 @@ t_r_curve_change_point = function(t_r_matrix, changepoint_method, max_cp_num, t_
 	print(dim(t_r_matrix))
 	### extract t value and r value
 	t_od = (t_r_matrix[,1])
-	r_od = log2(t_r_matrix[,2])-log2(t_r_matrix[,3]) ### log2 transform r
-
 	### round t for robustness
 	t_od_round = round(t_od / round_factor) * round_factor
+	### use convert data frame
+	t_r_matrix_df = as.data.frame(cbind(t_od_round, t_r_matrix[,2], t_r_matrix[,3]))
+	### add read sun with the same rounded t
+	t_r_matrix_round = aggregate(. ~ t_od_round, data=t_r_matrix_df, FUN=mean)
+
+	### get robust r
+	r_od = log2(t_r_matrix_round[,2]+2)-log2(t_r_matrix_round[,3]+2) ### log2 transform r
 
 	### remove x1 OR x2 equals 0
 	t = t_od_round[as.logical((!is.na(r_od)) * (is.finite(r_od)) ) ]
