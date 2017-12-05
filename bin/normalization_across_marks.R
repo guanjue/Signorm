5a9f8c3ab8f9f595d49970011fef5036cf6a99f1
GUANJUE XIANG <gzx103@E1-052111.science.psu.edu>
2017-12-05 14:24:17 -0500

    norm across marks

--- bin/normalization_across_marks.R
+++ bin/normalization_across_marks.R
@@ -5,20 +5,19 @@ print(mark_list_file)
 mark_list = read.table(mark_list_file, header = F)
 print(mark_list)
 median_list = c()
-i=1
-for (mark in mark_list){
+
+for (i in seq(1,length(mark_list))){
+	mark = mark_list[i]
 	print(mark)
 	d1=read.table(paste(mark, '.all_sample.txt', sep = ''), header =F)
 	### get non-zero median
 	median_data = median(d1[d1!=0,])
 	median_list[i] = median_data
-	i = i+1
 	print(median_data)
 }
 
 median_sf = max(median_list) / median_list
-i=1
-for (mark in mark_list){
+for (i in seq(1,length(mark_list))){
+	mark = mark_list[i]
 	write.table(median_sf[i], paste(mark, '.median_sf.txt', sep = ''), quote=FALSE, col.names=FALSE, row.names=FALSE, sep='\t')
-	i=i+1
 }
