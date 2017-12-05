484536cf0a21aac38f2053782a2047e933b13dc7
GUANJUE XIANG <gzx103@E1-052111.science.psu.edu>
2017-12-05 14:18:12 -0500

    norm across marks

--- bin/normalization_across_marks.R
+++ bin/normalization_across_marks.R
@@ -1,11 +1,11 @@
 args = commandArgs(trailingOnly=TRUE)
 mark_list_file = args[1]
 
-mark_list_file = read.table(mark_list_file, header = F)
+mark_list = read.table(mark_list_file, header = F)
 
 median_list = c()
 i=1
-for (mark in mark_list_file){
+for (mark in mark_list){
 	d1=read.table(paste(mark, '.all_sample.txt', sep = ''), header =F)
 	### get non-zero median
 	median_data = median(d1[d1!=0,])
@@ -16,7 +16,7 @@ for (mark in mark_list_file){
 
 median_sf = max(median_list) / median_list
 i=1
-for (mark in mark_list_file){
+for (mark in mark_list){
 	write.table(median_sf[i], paste(mark, '.median_sf.txt', sep = ''), quote=FALSE, col.names=FALSE, row.names=FALSE, sep='\t')
 	i=i+1
 }
