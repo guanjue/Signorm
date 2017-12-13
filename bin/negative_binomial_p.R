6646d11c9bad5b4288dae7861d7c15064bbca27e
Universe <universe@MacBook-Air.local>
2017-12-13 00:25:22 -0500

    add nb pval

--- bin/negative_binomial_p.R
+++ bin/negative_binomial_p.R
@@ -23,18 +23,18 @@ print(paste('check signal track overdispersion in background regions, var/mean='
 ### get negative binomial parameters from signal track bg regions
 sig_bg_prob = sig_bg_mean / sig_bg_var
 sig_bg_size = sig_bg_mean * sig_bg_prob / (1-sig_bg_prob)
-print(sig_bg_prob)
-print(sig_bg_size)
-
 ### get input bg regions
 input_bg = input[bg_bins[,1]==1,]
-input_bg_mean = mean(input_bg+1)
+input_bg_mean = mean(input_bg)
 inpy_bg_var = var(input_bg)
 print(paste('check input track overdispersion in background regions, var/mean=', toString(round(inpy_bg_var/input_bg_mean, digits=3)) ))
+print(sig_bg_prob)
+print(sig_bg_size)
+print(input_bg_mean)
 
 ### get negative binomial p-value
 sig_input = cbind(sig, input)
-nb_pval = apply(sig_input, MARGIN=1, function(x) 1-pnbinom(x[1], sig_bg_size * (x[2]+1)/input_bg_mean, sig_bg_prob) )
+nb_pval = apply(sig_input, MARGIN=1, function(x) 1-pnbinom(x[1], sig_bg_size * (x[2]+1)/(input_bg_mean+1), sig_bg_prob) )
 ### get -log10(p-value)
 neglog10_nb_pval = -log10(nb_pval)
 
