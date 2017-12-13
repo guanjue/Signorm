fb06793751e3270f063be4d6af73fa2857f3c091
Universe <universe@MacBook-Air.local>
2017-12-13 02:38:29 -0500

    add nb pval check mean var

--- bin/negative_binomial_p.R
+++ bin/negative_binomial_p.R
@@ -26,6 +26,9 @@ print(length(sig_bg_non0))
 
 ### get negative binomial parameters from signal track bg regions
 sig_bg_prob = sig_bg_mean / sig_bg_var
+if (sig_bg_prob<0.1){
+	sig_bg_prob = 0.1
+}
 sig_bg_size = sig_bg_mean * sig_bg_prob / (1-sig_bg_prob)
 ### get input bg regions
 input_bg = input[bg_bins[,1]==1,]
@@ -46,6 +49,7 @@ print(inpy_bg_var)
 sig_input = cbind(sig, input)
 nb_pval = apply(sig_input, MARGIN=1, function(x) 1-pnbinom(x[1], sig_bg_size * (x[2]+1)/(input_bg_mean+1), sig_bg_prob) )
 ### get -log10(p-value)
+nb_pval[nb_pval==0] = 0.1^16
 neglog10_nb_pval = -log10(nb_pval)
 
 ### write output
