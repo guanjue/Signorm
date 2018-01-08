63bfe488f9256dc51e165373916f491fa7789f16
Guanjue Xiang <gzx103@E1-052111.science.psu.edu>
2018-01-08 18:39:55 -0500

    add python version to get adj nbp

--- bin/negative_binomial_p_2r_bgadj.py
+++ bin/negative_binomial_p_2r_bgadj.py
@@ -73,7 +73,6 @@ def nbp_bg_adj(sample_sig_file, input_sig_file, outputname):
 			print(i)
 		i=i+1
 		nb_pval_tmp = np.array( 1 - nbinom.cdf(sig, sample_size, sample_prob, loc=0) )
-		print(nb_pval_tmp.shape)
 		nb_pval_list = np.concatenate((nb_pval_list, nb_pval_tmp))
 
 	############### second round
@@ -121,8 +120,9 @@ def nbp_bg_adj(sample_sig_file, input_sig_file, outputname):
 		if i%10000 == 0:
 			print(i)
 		i = i+1
-		nb_pval_tmp = 1 - nbinom.cdf(sig, sample_bg_size * (bg+1)/(background_bg_mean+1), sample_bg_prob, loc=0)
-		nb_pval_list = np.concatenate((nb_pval_list,np.array([nb_pval_tmp])))
+		nb_pval_tmp = np.array( 1 - nbinom.cdf(sig, sample_bg_size * (bg+1)/(background_bg_mean+1), sample_bg_prob, loc=0) )
+		nb_pval_list = np.concatenate((nb_pval_list, nb_pval_tmp))
+
 	### convert to np array
 	nb_pval_list = -np.log10(nb_pval_list)
 	nb_pval_list = nb_pval_list.reshape(nb_pval_list.shape[0], 1)
