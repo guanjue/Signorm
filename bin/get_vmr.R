3f25dc054a97fe423933c10efa82ea8e96aaa925
Universe <universe@MacBook-Air.local>
2018-01-20 16:55:12 -0500

    add 1/vmr track, add 0.001

--- bin/get_vmr.R
+++ bin/get_vmr.R
@@ -6,7 +6,7 @@ output_filename = args[2]
 data = read.table(input_matrix, header = F)
 
 ### get row 1 / variance-to-mean ratio (1/VMR) 
-data_vmr = apply(data, 1, function(x)  mean(x)+0.001 / var(x)+0.001 )
+data_vmr = apply(data, 1, function(x)  (mean(x)+0.001) / (var(x)+0.001) )
 
 ### get output
 write.table(data_vmr, output_filename, quote=FALSE, col.names=FALSE, row.names=FALSE, sep='\t')
