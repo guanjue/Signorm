5c825f5c3fc449c48982f1621d9a0ab1b86c1aa3
Universe <universe@MacBook-Air.local>
2018-01-20 16:15:29 -0500

    add 1/vmr track, add 0.001

---
+++ bin/get_vmr.R
@@ -0,0 +1,14 @@
+### get parameters
+args = commandArgs(trailingOnly=TRUE)
+input_matrix = args[1]
+output_filename = args[2]
+
+data = read.table(input_matrix, header = F)
+
+### get row 1 / variance-to-mean ratio (1/VMR) 
+data_vmr = apply(data, 1, function(x)  mean(x)+0.001 / var(x)+0.001 )
+
+### get output
+write.table(data_vmr, output_filename, quote=FALSE, col.names=FALSE, row.names=FALSE, sep='\t')
+
+
