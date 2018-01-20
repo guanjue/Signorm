da63e45939d7e0c5c1ec31f71d3f7dc3f7a70350
Universe <universe@MacBook-Air.local>
2018-01-20 15:29:51 -0500

    add 1/cv track

--- bin/get_cv.R
+++ bin/get_cv.R
@@ -5,8 +5,8 @@ output_filename = args[2]
 
 data = read.table(input_matrix, header = F)
 
-### get row Coefficient of Variation (CV)
-data_sv = apply(data, 1, function(x) sd(x) / mean(x))
+### get row 1 / Coefficient of Variation (1/CV)
+data_sv = apply(data, 1, function(x)  mean(x) / sd(x) )
 
 ### get output
 write.table(data_sv, output_filename, quote=FALSE, col.names=FALSE, row.names=FALSE, sep='\t')
