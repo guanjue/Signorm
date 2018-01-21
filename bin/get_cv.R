5594e2469003bcf439cfe3879e61e8815852ad0a
Universe <universe@MacBook-Air.local>
2018-01-21 15:29:36 -0500

    add small number before cv

--- bin/get_cv.R
+++ bin/get_cv.R
@@ -2,11 +2,13 @@
 args = commandArgs(trailingOnly=TRUE)
 input_matrix = args[1]
 output_filename = args[2]
-
+smallnum = as.numeric(args[3])
 data = read.table(input_matrix, header = F)
 
+### avoid negative value
+data = data + smallnum
 ### get row 1 / Coefficient of Variation (1/CV)
-data_sv = apply(data, 1, function(x)  (abs(mean(x))+0.1) / (sd(x)+0.1) )
+data_sv = apply(data, 1, function(x)  (abs(mean(x))) / (sd(x)) )
 
 ### get output
 write.table(data_sv, output_filename, quote=FALSE, col.names=FALSE, row.names=FALSE, sep='\t')
