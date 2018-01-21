649239b0485bba4890f21723fd66bd8f28338826
Universe <universe@MacBook-Air.local>
2018-01-21 15:55:05 -0500

    1-cv small num to 0.1

--- bin/get_cv.R
+++ bin/get_cv.R
@@ -7,8 +7,8 @@ data = read.table(input_matrix, header = F)
 
 ### avoid negative value
 data = data + smallnum
-### get row 1 / Coefficient of Variation (1/CV)
-data_sv = apply(data, 1, function(x)  mean(x) / sd(x) )
+### get row 1 - Coefficient of Variation (1-CV)
+data_sv = apply(data, 1, function(x)  1-mean(x)/sd(x) )
 
 ### get output
 write.table(data_sv, output_filename, quote=FALSE, col.names=FALSE, row.names=FALSE, sep='\t')
