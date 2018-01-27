4f5ae758f87ce9b2f3adc28e3d0d4b96932b9e84
Universe <universe@MacBook-Air.local>
2018-01-27 15:27:33 -0500

    rm add sn at sd and mean

--- bin/plot_sf_fsip.R
+++ bin/plot_sf_fsip.R
@@ -35,11 +35,11 @@ for (i in c(1:dim(info_list_files)[1])){
 
 
 ######### plot scale factor plot
-ymin = min(c(totalreads_sf, pk_sf, bg_sf))
-ymax = max(c(totalreads_sf, pk_sf, bg_sf))
+ymin_sf = min(c(totalreads_sf, pk_sf, bg_sf))
+ymax_sf = max(c(totalreads_sf, pk_sf, bg_sf))
 
 pdf(paste(outputname, '.sf.pdf', sep=''))
-plot(totalreads_sf, pch=20, col='black', ylim=c(ymin, ymax), axes=FALSE, xlab='')
+plot(totalreads_sf, pch=20, col='black', ylim=c(ymin_sf, ymax_sf), axes=FALSE, xlab='')
 axis(2)
 axis(1, at=c(1: length(cell_type)),labels=cell_type, las=2)
 points(pk_sf, pch=20, col='orange')
@@ -52,11 +52,27 @@ for (i in c(1:length(totalreads_sf))){
 }
 dev.off()
 
+ymin_FSiP = min(c(sig2_fsip, sig2_norm_fsip, sig1_fsip))-0.1
+ymax_FSiP = max(c(sig2_fsip, sig2_norm_fsip, sig1_fsip))+0.1
+
+pdf(paste(outputname, '.FSiP.pdf', sep=''))
+plot(sig2_fsip, pch=20, col='black', ylim=c(ymin_FSiP, ymax_FSiP), axes=FALSE, xlab='')
+axis(2)
+axis(1, at=c(1: length(cell_type)),labels=cell_type, las=2)
+points(sig2_norm_fsip, pch=20, col='orange')
+lines(sig1_fsip, pch=20, col='blue')
+for (i in c(1:length(sig2_norm_fsip))){
+	x_tmp = i
+	ymin = min(c(sig2_fsip[i], sig2_norm_fsip[i], sig1_fsip[i]))
+	ymax = max(c(sig2_fsip[i], sig2_norm_fsip[i], sig1_fsip[i]))
+	segments(x_tmp, ymin, x_tmp, ymax, col='gray', lwd=1.5)
+}
+dev.off()
 
 
 ######### plot FSiP factor plot
 FSiP = cbind(sig2_fsip, sig2_norm_fsip)
-pdf(paste(outputname, '.FSiP.pdf', sep=''))
+pdf(paste(outputname, '.FSiP.box.pdf', sep=''))
 boxplot(FSiP)#, ylim=c(0,0.8))
 dev.off()
 
