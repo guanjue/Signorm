96ab10b9ca474c44057cac24b0c287cf62e71d6e
Universe <universe@MacBook-Air.local>
2018-03-01 01:28:03 -0500

    FSiP

--- bin/FSiP.py
+++ bin/FSiP.py
@@ -33,7 +33,7 @@ def write2d_array(array,output):
 def FSiP(wg_bed, peak_bed, sig2_col_list, sig2_wg_raw):
 	sig2_col_list = sig2_col_list.split(',')
 
-	sig2_output_name = sig2_wg_raw.split('.')[0]
+	sig2_output_name = sig2_wg_raw.split('.')[0] + '.' + sig2_wg_raw.split('.')[1]
 
 	######
 	### get sig2 columns
@@ -55,11 +55,22 @@ def FSiP(wg_bed, peak_bed, sig2_col_list, sig2_wg_raw):
 	pk_table = read2d_array(sig2_output_name + '.pk.bed', str)
 	pk_sig = pk_table[:,4].astype(float)
 
-	print(np.sum(wg_sig))
-	print(np.sum(pk_sig))
+	pk_region_len = np.sum(pk_table[:,2].astype(float) - pk_table[:,1].astype(float))
+	pk_num = pk_sig.shape[0]
+	pk_sig_sum = np.sum(pk_sig)
+	wg_sig_sum = np.sum(wg_sig)
+	FSiP = pk_sig_sum / wg_sig_sum
 
-	call('rm ' + sig2_output_name + '.bed', shell=True)
-	call('rm ' + sig2_output_name + '.pk.bed', shell=True)
+	print('pk length: ' + str(pk_region_len))
+	print('pk num: ' + str(pk_num))
+	print('pk signal: ' + str(pk_sig_sum))
+	print('wg signal: ' + str(wg_sig_sum))
+	print('FSiP: ' + FSiP)
+
+	write2d_array([FSiP, pk_sig_sum, wg_sig_sum, pk_region_len, pk_num], sig2_output_name+'.info.txt')
+
+	#call('rm ' + sig2_output_name + '.bed', shell=True)
+	#call('rm ' + sig2_output_name + '.pk.bed', shell=True)
 ############################################################################
 #time python /Volumes/MAC_Data/data/labs/zhang_lab/01projects/signorm/bin/peaknorm_order.py -w 200_noblack.11_22_2017.bed -p atacTable180124peaksFiltered.txt -n 100000 -a $sig1_col -b $sig1'.upperlim.txt' -c $sig2_col -d $sig2'.upperlim.txt' -u 32
 
