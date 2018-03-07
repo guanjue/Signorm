7897bc110161055e0f51490ff393d1de228416fa
Universe <universe@client-130-203-161-61.mobility.psu.edu>
2018-03-07 18:34:25 -0500

    clean sth

--- bin/transition_matrix_ct.py
+++ bin/transition_matrix_ct.py
@@ -15,7 +15,7 @@ def read2d_array(filename,dtype_used, split='\t'):
 	data=open(filename,'r')
 	data0=[]
 	for records in data:
-		tmp = [x.strip() for x in records.split(' ')]
+		tmp = [x.strip() for x in records.split(split)]
 		data0.append(tmp)
 	data0 = np.array(data0,dtype=dtype_used)
 	data.close()
@@ -32,25 +32,37 @@ def write2d_array(array,output):
 	r1.close()
 
 
+cd_tree_file = 'cd_tree.txt'
+cd_tree = read2d_array(cd_tree_file, str, ',') 
+
 
 state_input = 'test_20cell.location_pknorm_16lim_refmean_samplemean_nosub.state'
 
-state_all = read2d_array(state_input, str, ' ') 
-state = state_all[1:,4:state_all.shape[1]-1].astype(int)
+state_input = 'test.state'
 
-state_freq = np.bincount(state.flatten())
-state_freq = np.unique(state, return_counts=True)
-
+state_all = read2d_array(state_input, str, ' ')
+state_all_ct = state_all[0]
 
 kmer_count = {}
-for i in range(0, state.shape[1]):
-	wg = state[:,i]
-	for j in range(0, wg.shape[0]-1):
-		kmer_ij = str(wg[j]) + str(wg[j+1])
+for ct in cd_tree:
+	s = ct[0]
+	t = ct[1]
+	s_col_id = np.where(state_all_ct==s)[0][0]
+	t_col_id = np.where(state_all_ct==t)[0][0]
+	s_state_array = state_all[1:,s_col_id]
+	t_state_array = state_all[1:,t_col_id]
+	for s_state,t_state in zip(s_state_array, t_state_array):
+		kmer_ij = str(s_state) + str(t_state)
 		if kmer_ij in kmer_count:
 			kmer_count[kmer_ij] = kmer_count[kmer_ij] + 1
 		else:
 			kmer_count[kmer_ij] = 1
+
+
+state = state_all[1:,4:state_all.shape[1]-1].astype(int)
+
+state_freq = np.bincount(state.flatten())
+state_freq = np.unique(state, return_counts=True)
 
 
 state_transition_matrix = []
@@ -74,13 +86,16 @@ for i in range(0,state_freq[0].shape[0]):
 
 state_transition_matrix = np.array(state_transition_matrix, dtype=float)
 
-write2d_array(state_transition_matrix, 'state_transition_matrix.txt')
+write2d_array(state_transition_matrix, 'state_transition_matrix_ct.txt')
 
 fig, ax = plt.subplots(figsize=(10,10))  
-seaborn.clustermap(state_transition_matrix, annot=True, linewidths=.5, ax=ax)
-fig.savefig('state_transition_matrix.png')
+seaborn.heatmap(state_transition_matrix, annot=True, linewidths=.5)
+fig.savefig('state_transition_matrix_ct.png')
 
 
+fig, ax = plt.subplots(figsize=(10,10))  
+fig = seaborn.clustermap(state_transition_matrix)
+fig.savefig('state_transition_matrix_ct.png')
 
 
 
