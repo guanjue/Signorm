a9eeb71d6e37aa7e8a491b9ccac53454a3191215
Universe <universe@client-75-102-112-41.mobility.psu.edu>
2018-03-16 13:35:09 -0400

    back to z score

--- bin/pknorm_common.py
+++ bin/pknorm_common.py
@@ -106,7 +106,7 @@ def pknorm(sig1_wg_raw, sig2_wg_raw, moment, B_init, fdr_thresh, sample_num, sma
 	sig1 = read2d_array(sig1_wg_raw, float)
 	sig2 = read2d_array(sig2_wg_raw, float)
 	
-	p_method = 'nb'
+	p_method = 'z'
 	### read whole genome binary label
 	if p_method == 'nb':
 		sig1_p = read2d_array(sig1_wg_raw + '.nbp.txt', float)
@@ -128,7 +128,7 @@ def pknorm(sig1_wg_raw, sig2_wg_raw, moment, B_init, fdr_thresh, sample_num, sma
 
 	print(sig1_pk_num)
 
-	if p_method == 'nb':
+	if p_method == 'z':
 		sig2_p = read2d_array(sig2_wg_raw + '.nbp.txt', float)
 		sig2_z_p_fdr = p_adjust(sig2_p, 'fdr')
 	elif p_method == 'nb_in':
