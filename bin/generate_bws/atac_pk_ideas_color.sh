e0aea4a89a47075218b1528644635ec016aabee3
Universe <universe@MacBook-Air.local>
2018-04-04 00:06:10 -0400

    check counts

--- bin/generate_bws/atac_pk_ideas_color.sh
+++ bin/generate_bws/atac_pk_ideas_color.sh
@@ -3,7 +3,7 @@ wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/bedToBigBed
 chmod 777 bedToBigBed
 
 
-time python ~/group/software/signorm/bin/generate_bws/atac_pk_ideas_color.py -a /storage/home/gzx103/scratch/vision/5end/index_set_20cell_pknorm/snapshot18/atac_20cell.index.matrix.txt -b 5 -c /storage/home/gzx103/scratch/vision/5end/index_set_20cell_pknorm/peak_list.txt -d /storage/home/gzx103/scratch/vision/5end/index_set_20cell_pknorm/snapshot18/atac_20cell.function.matrix.txt -e 5 -f /storage/home/gzx103/scratch/vision/5end/index_set_20cell_pknorm/ideas_list.txt -g /storage/home/gzx103/scratch/vision/5end/index_set_20cell_pknorm/ideas_atac_sig_color/ideas_state_id_color_name_list.txt -o atac_pk
+time python ~/group/software/signorm/bin/generate_bws/atac_pk_ideas_color.py -a /storage/home/gzx103/scratch/vision/5end/index_set_20cell_pknorm/snapshot20/atac_20cell.index.matrix.txt -b 5 -c /storage/home/gzx103/scratch/vision/5end/index_set_20cell_pknorm/peak_list.txt -d /storage/home/gzx103/scratch/vision/5end/index_set_20cell_pknorm/snapshot20/atac_20cell.function.matrix.txt -e 5 -f /storage/home/gzx103/scratch/vision/5end/index_set_20cell_pknorm/ideas_list.txt -g /storage/home/gzx103/scratch/vision/5end/index_set_20cell_pknorm/ideas_atac_sig_color/ideas_state_id_color_name_list.txt -o atac_pk
 
 ls atac_pk.*.bed > atac_pk_ideas_state_color_file_list.txt
 
