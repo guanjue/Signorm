7ce94c7e2ea085414b392f2efde9cea0c2ae073a
Universe <universe@MacBook-Air.local>
2018-04-30 17:44:37 -0400

    get exp sort bed pc only

--- bin/get_expression_sorted_bed.sh
+++ bin/get_expression_sorted_bed.sh
@@ -1,13 +1,20 @@
-
+### get cmp_ery reads count table
 tail -n+2 rnaHtseqCountsall.txt | awk -F '\t' -v OFS='\t' '{print $1, ($6+$7)/2, ($8+$9)/2}' > rnaHtseqCountsall.cmp_ery.txt
 
+### get protein coding gene only
+cat gencode.vM4.annotation.bed | awk -F '\t' -v OFS='\t' '{if ($5=="protein_coding") print $0}' > gencode.vM4.annotation.pc.bed
+
+### sort cmp_ery reads count table by matched bed id
 time python ~/group/software/signorm/bin/vlookup.py -t rnaHtseqCountsall.cmp_ery.txt -m 1 -s gencode.vM4.annotation.bed -n 4 -o rnaHtseqCountsall.cmp_ery.bedmatched.tmp1.txt
 
+### get expression signal only
 cut -f2,3 rnaHtseqCountsall.cmp_ery.bedmatched.tmp1.txt > rnaHtseqCountsall.cmp_ery.bedmatched.tmp2.txt
 
-paste gencode.vM4.annotation.bed rnaHtseqCountsall.cmp_ery.bedmatched.tmp2.txt | awk -F '\t' -v OFS='\t' '{print $1,$2,$3,$4,$5,$6, ($7+10)/($8+10)}' | sort -k7,7n > rnaHtseqCountsall.cmp_ery.bedmatched.txt
+### paste bed info & sort by fold-change between cmp & ery_fl
+paste gencode.vM4.annotation.pc.bed rnaHtseqCountsall.cmp_ery.bedmatched.tmp2.txt | awk -F '\t' -v OFS='\t' '{print $1,$2,$3,$4,$5,$6, ($7+10)/($8+10)}' | sort -k7,7n > rnaHtseqCountsall.cmp_ery.bedmatched.txt
 
-cut -f1,2,3,4,5,6 rnaHtseqCountsall.cmp_ery.bedmatched.txt > gencode.vM4.annotation.cmp_ery_fc.bed
+### get sorted bed file
+cut -f1,2,3,4,5,6 rnaHtseqCountsall.cmp_ery.bedmatched.txt > gencode.vM4.annotation.pc.cmp_ery_fc.bed
 
 rm rnaHtseqCountsall.cmp_ery.bedmatched.tmp1.txt
 rm rnaHtseqCountsall.cmp_ery.bedmatched.tmp2.txt
